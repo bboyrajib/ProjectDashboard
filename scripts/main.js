77f6421e1c49e4497b403e4d08edfa3b84f8d778
bboyrajib <bboyrajib@gmail.com>
2020-06-13 18:09:53 +0530

    Alignment changes & added scrollbar to address

--- scripts/main.js
+++ scripts/main.js
@@ -52,7 +52,6 @@ window.onload = function(){
 function changeProfilePic(uploader) {
 
     if ( uploader.files && uploader.files[0] ){
-    	console.log(uploader.files[0])
           $('#profileImage').attr('src', 
              window.URL.createObjectURL(uploader.files[0]) );
     }
